e230dedc91f2cdffe551e091bd4de523a21f0a12
leeeesanggyu <76906458+leeeesanggyu@users.noreply.github.com>
2023-11-29 17:32:26 +0900

    fix: Props 명 변경 및 type 확장자 변경

--- src/main/post.tsx
+++ src/main/post.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-type PostProps = {
+type Props = {
     title: string;
 }
 
-function Post(postProps: PostProps) {
+function Post(props: Props) {
     return <article>
         <h2>Post</h2>
-        <li><a href="/post/1">{postProps.title} 1</a></li>
-        <li><a href="/post/2">{postProps.title} 2</a></li>
+        <li><a href="/post/1">{props.title} 1</a></li>
+        <li><a href="/post/2">{props.title} 2</a></li>
     </article>
 }
 
