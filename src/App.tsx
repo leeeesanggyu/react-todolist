3a5ceea644370815f3ab80a85c5a223561eda4f2
leeeesanggyu <76906458+leeeesanggyu@users.noreply.github.com>
2023-11-29 14:34:22 +0900

    feat: Component 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Header() {
+    return <header>
+        <h2>안녕하세요, 이상규의 투두리스트 입니다.</h2>
+    </header>
+}
+
+function Main() {
+    return <nav>
+        <p><a href="/">메인 이동 샤샥</a></p>
+    </nav>
+}
+
+function Post() {
+    return <article>
+        <li><a href="/post/1">게시물 1</a></li>
+        <li><a href="/post/2">게시물 2</a></li>
+    </article>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Header></Header>
+        <Main></Main>
+        <Post></Post>
     </div>
   );
 }
