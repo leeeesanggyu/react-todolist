9a87a26b190f52beb42c058988feb41a90329819
leeeesanggyu <76906458+leeeesanggyu@users.noreply.github.com>
2023-12-30 21:29:38 +0900

    feat: 완료한 To-do 클릭시 line-through 기능 추가

--- src/todo/TodoList/Todo/Todo.tsx
+++ src/todo/TodoList/Todo/Todo.tsx
@@ -1,4 +1,4 @@
-import React, {PropsWithChildren} from "react";
+import React, {useState} from "react";
 import {TodoItem} from "../../type/TodoItem";
 
 type Props = {
@@ -7,9 +7,22 @@ type Props = {
 }
 
 export default function Todo(props: Props) {
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleClick = () => {
+        setIsClicked(!isClicked);
+    };
+
+    const textStyle = {
+        textDecoration: isClicked ? 'line-through' : 'none',
+        cursor: 'pointer',
+    };
+
     return (
         <div>
-            {props.todo.text}
+            <span onClick={handleClick} style={textStyle}>
+                {props.todo.text}
+            </span>
             <input
                 onChange={() => props.deleteFlagChange(props.todo.id)}
                 type="checkbox"
